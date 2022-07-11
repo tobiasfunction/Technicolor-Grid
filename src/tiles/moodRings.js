44cfd0ddb728627ec308f193eed3f1f472e5485c
Tobias Tobak <tobiasfunction@protonmail.com>
2022-07-10 21:07:42 -0400

    Added listener tile, tweaked colors

--- src/tiles/moodRings.js
+++ src/tiles/moodRings.js
@@ -5,27 +5,38 @@ export default function (props) {
   const size = "170%";
 
   return (
-    <div
-      className="tile"
-      style={{
-        backgroundColor: color,
-        borderRadius: "50%",
-        boxShadow: "0px 0px 6px #fff inset",
-        gridColumn: props.column,
-        gridRow: props.row,
-        height: size,
-        mixBlendMode: "difference",
-        opacity: "100%",
-        width: size,
-        overflow: "hidden",
-      }}
-      onMouseEnter={mouseEnter}
-    />
+    <>
+      <div
+        className="tile"
+        style={{
+          backgroundColor: color,
+          borderRadius: "50%",
+          boxShadow: "0 0 6px #fff inset, 0 0 8px #fff inset",
+          gridColumn: props.column,
+          gridRow: props.row,
+          height: size,
+          mixBlendMode: "difference",
+          opacity: "100%",
+          width: size,
+          zIndex: "100",
+        }}
+      />
+      <div // Listener tile
+        className="tile"
+        onMouseEnter={mouseEnter}
+        style={{
+          gridColumn: props.column,
+          gridRow: props.row,
+          zIndex: "200",
+        }}
+      />
+    </>
   );
+
   function mouseEnter() {
     const h = Math.floor(Math.random() * 360);
     const s = Math.floor(Math.random() * 5) + 20;
-    const l = Math.floor(Math.random() * 10) + 70;
+    const l = Math.floor(Math.random() * 30) + 70;
     setColor(`hsla(${h}, ${s}%, ${l}%, 1)`);
   }
 }
